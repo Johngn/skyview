14672d49a53890437fdb9d2c61caa363c742c82d
John Gillan <jgillan@protonmail.com>
2022-10-30 22:49:59 +0000

    Switched to roadgoat

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
-import AutoCompleteContainer from './components/AutoCompleteContainer';
 import Chart from './components/Chart';
 import Daypicker from './components/Daypicker';
 import LoadingSpinner from './components/LoadingSpinner';
@@ -14,9 +12,10 @@ import './App.css';
 import './datepickercustomstyles.css';
 
 const App = () => {
-  const [enteredAddress, setEnteredAddress] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
   const [lat, setLat] = useState(0);
   const [lng, setLng] = useState(0);
+  const [autoCompleteResults, setAutoCompleteResults] = useState([]);
 
   const [forecastTimes, setForecastTimes] = useState([]);
   const [cloudArray, setCloudArray] = useState([]);
@@ -28,70 +27,70 @@ const App = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (enteredAddress.length > 10) {
-      setLoading(true);
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`
-        )
-        .then(response => {
-          selectedDate.setHours(23, 0, 0, 0);
+    setLoading(true);
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`
+      )
+      .then(response => {
+        selectedDate.setHours(23, 0, 0, 0);
 
-          const weatherTonight = response.data.list.filter(
-            (weathersingle: any) => {
-              return (
-                new Date(weathersingle.dt * 1000).getTime() >=
-                  selectedDate.getTime() - 3600000 * 6 &&
-                new Date(weathersingle.dt * 1000).getTime() <=
-                  selectedDate.getTime() + 3600000 * 12
-              );
-            }
-          );
+        const weatherTonight = response.data.list.filter(
+          (weathersingle: any) => {
+            return (
+              new Date(weathersingle.dt * 1000).getTime() >=
+                selectedDate.getTime() - 3600000 * 6 &&
+              new Date(weathersingle.dt * 1000).getTime() <=
+                selectedDate.getTime() + 3600000 * 12
+            );
+          }
+        );
 
-          setForecastTimes(
-            weatherTonight.map((hr: any) => hr.dt_txt.substring(11, 16))
-          );
-          setCloudArray(weatherTonight.map((hr: any) => hr.clouds.all));
-          setHumidityArray(weatherTonight.map((hr: any) => hr.main.humidity));
-          setWindArray(weatherTonight.map((hr: any) => hr.wind.speed));
+        setForecastTimes(
+          weatherTonight.map((hr: any) => hr.dt_txt.substring(11, 16))
+        );
+        setCloudArray(weatherTonight.map((hr: any) => hr.clouds.all));
+        setHumidityArray(weatherTonight.map((hr: any) => hr.main.humidity));
+        setWindArray(weatherTonight.map((hr: any) => hr.wind.speed));
 
-          setLoading(false);
-        })
-        .catch(error => {
-          setError(error.message);
-          setLoading(false);
-        });
-
-      return setLoading(true);
-    }
-  }, [lat, lng, selectedDate]);
-
-  const onAddressChangeHandler = (enteredAddress: string) => {
-    setEnteredAddress(enteredAddress);
-  };
-
-  const onAddressSelectHandler = () => {
-    geocodeByAddress(enteredAddress)
-      .then(results => getLatLng(results[0]))
-      .then(latLng => {
-        setLat(latLng.lat);
-        setLng(latLng.lng);
+        setLoading(false);
       })
       .catch(error => {
         setError(error.message);
+        setLoading(false);
       });
-  };
 
-  const inputProps = {
-    value: enteredAddress,
-    placeholder: 'Search Location',
-    onChange: onAddressChangeHandler,
-    onSelect: onAddressSelectHandler,
-    autoFocus: true,
-  };
+    return setLoading(true);
+  }, [lat, lng, selectedDate]);
 
   const clearError = () => {
     setError(null);
+  };
+
+  const auth_key = Buffer.from(
+    `${process.env.REACT_APP_ROADGOAT_ACCESS_KEY}:${process.env.REACT_APP_ROADGOAT_SECRET_KEY}`
+  ).toString('base64');
+
+  const changeSearchTerm = (text: string) => {
+    setSearchTerm(text);
+    axios
+      .get(
+        `https://api.roadgoat.com/api/v2/destinations/auto_complete?q=${text}`,
+        {
+          headers: {
+            Authorization: `Basic ${auth_key}`,
+          },
+        }
+      )
+      .then(res => {
+        const result = res.data?.data?.map(({ attributes, id }: any) => ({
+          id: id,
+          long_name: attributes.long_name,
+          latitude: attributes.latitude,
+          longitude: attributes.longitude,
+        }));
+        setAutoCompleteResults(result ? result : []);
+      });
   };
 
   return (
@@ -103,7 +102,30 @@ const App = () => {
         <Introduction />
         <form className="searchform">
           <div style={{ width: '60%' }}>
-            <AutoCompleteContainer inputProps={inputProps} />
+            <input
+              className="searchInput"
+              value={searchTerm}
+              onChange={e => changeSearchTerm(e.target.value)}
+            />
+            <div className="autocomplete-container">
+              {autoCompleteResults.map(
+                ({ long_name, latitude, longitude, id }) => (
+                  <button
+                    key={id}
+                    className="autocomplete-item-button"
+                    onClick={e => {
+                      e.preventDefault();
+                      setLat(latitude);
+                      setLng(longitude);
+                      setAutoCompleteResults([]);
+                      setSearchTerm(long_name);
+                    }}
+                  >
+                    {long_name}
+                  </button>
+                )
+              )}
+            </div>
           </div>
           <Daypicker
             selectedDate={selectedDate}
--- src/components/AutoCompleteContainer.js
+++
@@ -1,7 +0,0 @@
-import PlacesAutocomplete from 'react-places-autocomplete';
-
-const AutoCompleteContainer = ({ inputProps }) => {
-  return <PlacesAutocomplete inputProps={inputProps} />;
-};
-
-export default AutoCompleteContainer;
