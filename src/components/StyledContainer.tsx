cfcd5a341564c0d8aa68ca3456ae8742f5bf729c
John Gillan <jgillan@protonmail.com>
2022-10-30 23:03:00 +0000

    Change background image

--- src/components/StyledContainer.tsx
+++ src/components/StyledContainer.tsx
@@ -2,11 +2,10 @@ import styled from 'styled-components';
 
 const StyledContainer = styled.div`
   max-width: 800px;
-  width: 100%;
   margin: 20px auto;
   border: 1px solid black;
   padding: 20px;
-  background-color: rgb(51, 58, 83);
+  background-color: rgba(51, 58, 83, 0.8);
   box-shadow: 1px 1px 5px black;
   border-radius: 5px;
 `;
